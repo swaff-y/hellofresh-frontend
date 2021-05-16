8e3d21bd8970304e8c83d6756b46ea03942fe601
swaff-y <kyle@swaff.id.au>
2021-05-16 18:31:14 +1000

    url change for deployment

--- src/api.js
+++ src/api.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 // http://localhost:3000/
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: "https://hello-fresh-backend.herokuapp.com/",
 })
 
 export default api;
