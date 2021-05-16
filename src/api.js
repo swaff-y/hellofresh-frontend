3b7cb831a4c0d1aab3b15bb95178de602d00e85f
swaff-y <kyle@swaff.id.au>
2021-05-16 16:07:50 +1000

    api local host

--- src/api.js
+++ src/api.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 // https://hello-fresh-backend.herokuapp.com/
-// https://localhost:3000/
+// http://localhost:3000/
 
 const api = axios.create({
-  baseURL: "https://localhost:3000/",
+  baseURL: "http://localhost:3000/",
 })
 
 export default api;
